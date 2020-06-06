2cfc35dcd35ecfa98b1facf7d6adec61f6782849
Mahmoud Gatfi <mahmoud.gatfi@schibsted.com>
2020-06-06 14:47:06 +0200

    Components set up and style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="appContainer" className="container">
+      <div className="App">
+        <h1 id="appHeader">Pomodro Clock</h1>
+        <div id="timer-label">New Session</div>
+        <div id="time-left">25:00</div>
+        <div className="row">
+          <i id="start_stop" class="far fa-play-circle fa-2x"></i>
+          <i id="reset" class="fas fa-power-off fa-2x"></i>
+        </div>
+        <div className="row">
+          <div className="card timeLength">
+            <div id="session-label" className="timeLengthLabel">
+              Session length
+            </div>
+            <div id="session-length" className="timeLengthValue">
+              25
+            </div>
+            <i id="session-increment" className="fas fa-arrow-circle-up"></i>
+            <i id="session-decrement" className="fas fa-arrow-circle-down"></i>
+          </div>
+          <div className="card timeLength">
+            <div id="break-label" className="timeLengthLabel">
+              Break length
+            </div>
+            <div id="break-length" className="timeLengthValue">
+              5
+            </div>
+            <i id="break-increment" className="fas fa-arrow-circle-up"></i>
+            <i id="break-decrement" className="fas fa-arrow-circle-down"></i>
+          </div>
+        </div>
+        <div className="card-footer">by Mahmoud Gatfi</div>
+      </div>
     </div>
   );
 }
